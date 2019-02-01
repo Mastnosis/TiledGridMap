076dbd7605c750f2e16ee50c4cd2605d33425a27
icarus <icarus@mindmagic.ca>
2019-02-01 13:40:16 -0600

    changed toString on Coordinate to more condensed (row/col)

--- src/ca/mindmagic/game/map/grid/Coordinate.kt
+++ src/ca/mindmagic/game/map/grid/Coordinate.kt
@@ -1,3 +1,7 @@
 package ca.mindmagic.game.map.grid
 
-data class Coordinate(val row: Int, val col: Int)
+data class Coordinate(val row: Int, val col: Int) {
+    override fun toString(): String {
+        return "($row/$col)"
+    }
+}
