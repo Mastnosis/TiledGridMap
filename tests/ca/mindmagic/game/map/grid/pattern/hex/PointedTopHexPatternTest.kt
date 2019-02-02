6b0315015ec80acf8859cae2a44d087801563584
icarus <icarus@mindmagic.ca>
2019-02-01 19:03:13 -0600

    add range test for when range is greater that the delta for rows or cols.

--- tests/ca/mindmagic/game/map/grid/pattern/hex/PointedTopHexPatternTest.kt
+++ tests/ca/mindmagic/game/map/grid/pattern/hex/PointedTopHexPatternTest.kt
@@ -1,9 +1,12 @@
 package ca.mindmagic.game.map.grid.pattern.hex
 
 import ca.mindmagic.game.map.grid.Coordinate
-import org.junit.Test
+import org.junit.Assert.assertArrayEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
 
-import org.junit.Assert.*
+
+import kotlin.test.*
 
 class PointedTopHexPatternTest {
 
@@ -17,6 +20,11 @@ class PointedTopHexPatternTest {
     }
 
     @Test
+    fun rangeGreaterThanDeltaRowDeltaCol() {
+        assertEquals(3, pointedHexPattern.range(Coordinate(0, 1), Coordinate(1, -2)))
+    }
+
+    @Test
     fun rangeNeighbors() {
         assertEquals(1, pointedHexPattern.range(origin, Coordinate(0, 1)))
         assertEquals(1, pointedHexPattern.range(origin, Coordinate(-1, 0)))
@@ -26,15 +34,6 @@ class PointedTopHexPatternTest {
         assertEquals(1, pointedHexPattern.range(origin, Coordinate(-1, -1)))
     }
 
-    @Test
-    fun rangeNeighborsNeighbor() {
-        assertEquals(2, pointedHexPattern.range(origin, Coordinate(1, 1)))
-    }
-
-    @Test
-    fun rangeThree() {
-        assertEquals(3, pointedHexPattern.range(origin, Coordinate(2, 2)))
-    }
 
     @Test
     fun rangeToNegCoordinate() {
