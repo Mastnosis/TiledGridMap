07c2b64031689b56a1594550e4584341e97e3d03
icarus <icarus@mindmagic.ca>
2019-02-01 18:40:56 -0600

    add gridmap range calculations that allow for edge wrapping.

--- src/ca/mindmagic/game/map/grid/GridMap.kt
+++ src/ca/mindmagic/game/map/grid/GridMap.kt
@@ -7,8 +7,14 @@ open class GridMap @JvmOverloads constructor(pattern: Pattern,
                                              _width: Int, _height: Int,
                                              val wrapHorizontal: Boolean = false, val wrapVertical: Boolean = false) : Grid(pattern) {
 
-    val width = if (_width < 1) 1 else _width
+    /*
+        for wrapped maps, it is important to have even dimensions in order for edges to match up cleanly
+        neighborsOf and range will possibly give undefined behavior if dimension is odd
+     */
+    // TODO consider forcing dimensions to be even (+ _height % 2)
     val height = if (_height < 1) 1 else _height
+    val width = if (_width < 1) 1 else _width
+
 
 
     override fun neighborsOf(location: Coordinate): Set<Coordinate> {
@@ -28,6 +34,26 @@ open class GridMap @JvmOverloads constructor(pattern: Pattern,
         return true
     }
 
+    /*
+        It is important to note that wrapped maps need special consideration for range calculations as there is
+        more than one possible path from source to target
+     */
+    override fun range(source: Coordinate, target: Coordinate): Int {
+        // TODO optimize for use with wrapped edges. Should at least cap iterations of loop at max(width, height)
+        return super.universalRange(source, target)  // quick fix but not optimized
+    }
+
+    private fun wrappedRange(source: Coordinate, target: Coordinate): Int {
+        var range = range(source, target)
+        if (wrapHorizontal) {
+
+        }
+        if (wrapVertical) {
+
+        }
+        return range
+    }
+
     private fun wrapIfNeeded(c: Coordinate): Coordinate {
         var result = c
         if (wrapVertical) result = wrapVertical(result)
@@ -38,7 +64,7 @@ open class GridMap @JvmOverloads constructor(pattern: Pattern,
     private fun wrapVertical(c: Coordinate): Coordinate {
         var yOffset = c.row
         while (yOffset < 0) {
-            yOffset = height - yOffset       // it is important that height never be less than 1 or this will never exit
+            yOffset = height + yOffset       // it is important that height never be less than 1 or this will never exit
         }
         while (yOffset >= height) {
             yOffset -= height
@@ -49,7 +75,7 @@ open class GridMap @JvmOverloads constructor(pattern: Pattern,
     private fun wrapHorizontal(c: Coordinate): Coordinate {
         var xOffset = c.col
         while (xOffset < 0) {
-            xOffset = width - xOffset       // it is important that width never be less than 1 or this will never exit
+            xOffset = width + xOffset       // it is important that width never be less than 1 or this will never exit
         }
         while (xOffset >= width) {
             xOffset -= width
